de97be84afcbf31668b6a7f837ecef75af10c02b
jovito-royeca <jovit.royeca@gmail.com>
2016-01-11 16:16:25 +0800

    Implemented the echo and reverb buttons.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -11,9 +11,12 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController/*, AVAudioPlayerDelegate*/ {
     var audioPlayer:AVAudioPlayer!
+    var echoPlayer:AVAudioPlayer!
+    var reverbPlayers:[AVAudioPlayer] = []
     var audioEngine:AVAudioEngine!
     var audioFile:AVAudioFile!
     var receivedAudio:RecordedAudio!
+    let N:Int = 10 // number of reverb players
     
     @IBOutlet weak var slowButton: UIButton!
     @IBOutlet weak var fastButton: UIButton!
@@ -24,6 +27,12 @@ class PlaySoundsViewController: UIViewController/*, AVAudioPlayerDelegate*/ {
         super.viewDidLoad()
 
         try! audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
+        try! echoPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
+        for _ in 0...N {
+            var temp:AVAudioPlayer
+            try! temp = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
+            reverbPlayers.append(temp)
+        }
 //        audioPlayer.delegate = self
         
         audioEngine = AVAudioEngine()
@@ -37,31 +46,27 @@ class PlaySoundsViewController: UIViewController/*, AVAudioPlayerDelegate*/ {
     
 
     @IBAction func playSlowAudio(sender: UIButton) {
-        playbackStop()
         playAudio(0.5)
     }
     
     @IBAction func playFastAudio(sender: UIButton) {
-        playbackStop()
         playAudio(1.5)
     }
     
     @IBAction func playChipmunk(sender: UIButton) {
-        playbackStop()
         playAudioWithVariablePitch(1000)
     }
     
     @IBAction func playDarthVader(sender: UIButton) {
-        playbackStop()
         playAudioWithVariablePitch(-1000)
     }
     
     @IBAction func playEcho(sender: UIButton) {
-        
+        playEcho()
     }
     
     @IBAction func playReverb(sender: UIButton) {
-        
+        playReverb()
     }
     
     @IBAction func playStop(sender: UIButton) {
@@ -70,24 +75,30 @@ class PlaySoundsViewController: UIViewController/*, AVAudioPlayerDelegate*/ {
     
     func playbackStop() {
         audioPlayer.stop()
+        audioPlayer.currentTime = 0.0
+        
+        echoPlayer.stop()
+        echoPlayer.currentTime = 0.0
+        
+        for i in 0...N {
+            let player:AVAudioPlayer = reverbPlayers[i]
+            player.stop()
+            player.currentTime = 0.0
+        }
+        
         audioEngine.stop()
         audioEngine.reset()
     }
     
     func playAudio(rate: Float) {
-        audioEngine.stop()
-        audioEngine.reset()
-        audioPlayer.stop()
+        playbackStop()
         
         audioPlayer.rate = rate
-        audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
     
     func playAudioWithVariablePitch(pitch: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        playbackStop()
         
         let audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
@@ -105,6 +116,36 @@ class PlaySoundsViewController: UIViewController/*, AVAudioPlayerDelegate*/ {
         
         audioPlayerNode.play()
     }
+    
+    func playEcho() {
+        playbackStop()
+        
+        audioPlayer.play()
+        
+        let delay:NSTimeInterval = 0.1//100ms
+        var playtime:NSTimeInterval
+        playtime = echoPlayer.deviceCurrentTime + delay
+        echoPlayer.volume = 0.8;
+        echoPlayer.playAtTime(playtime)
+    }
+    
+    func playReverb() {
+        playbackStop()
+        
+//        20ms produces detectable delays
+        let delay:NSTimeInterval = 0.02
+        for i in 0...N {
+            let curDelay:NSTimeInterval = delay*NSTimeInterval(i)
+            let player:AVAudioPlayer = reverbPlayers[i]
+            //M_E is e=2.718...
+            //dividing N by 2 made it sound ok for the case N=10
+            let exponent:Double = -Double(i)/Double(N/2)
+            let volume = Float(pow(Double(M_E), exponent))
+            player.volume = volume
+            player.playAtTime(player.deviceCurrentTime + curDelay)
+        }
+    }
+    
     /*
     func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
         playStop()
