5cb2ac2af073e5ea721585d0c572ceaa2c1e9246
jovito-royeca <jovit.royeca@gmail.com>
2016-01-12 09:05:38 +0800

    Implemented pause and resume.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -12,12 +12,14 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
     var audioPlayer:AVAudioPlayer!
     var audioUpdater:CADisplayLink!
-    
     var echoPlayer:AVAudioPlayer!
+    var audioPlayerNode:AVAudioPlayerNode!
     var audioEngine:AVAudioEngine!
     var audioFile:AVAudioFile!
     var receivedAudio:RecordedAudio!
     var activeButton:UIButton?
+    var pauseButtonImage: UIImage!
+    var resumeButtonImage: UIImage!
     
 //MARK: UI elements
     @IBOutlet weak var slowButton: UIButton!
@@ -41,6 +43,9 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         
         audioEngine = AVAudioEngine()
         try! audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl)
+        
+        pauseButtonImage = UIImage(named: "pauseButton")
+        resumeButtonImage = UIImage(named: "resumeButton")
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -48,6 +53,10 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         resetPlaybackControls(audioPlayer.duration)
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        playbackStop()
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -55,104 +64,95 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
 
 //MARK: button actions
     @IBAction func playSlowAudio(sender: UIButton) {
-        playAudioWithRate(0.5)
+        if (activeButton == sender) {
+            if (audioPlayer.playing) {
+                playbackPause()
+            } else {
+                playAudioWithRate(0.5)
+            }
+            
+        } else {
+            playbackStop()
+            activeButton = sender
+            playAudioWithRate(0.5)
+        }
     }
     
     @IBAction func playFastAudio(sender: UIButton) {
-        playAudioWithRate(1.5)
+        if (activeButton == sender) {
+            if (audioPlayer.playing) {
+                playbackPause()
+            } else {
+                playAudioWithRate(1.5)
+            }
+        } else {
+            playbackStop()
+            activeButton = sender
+            playAudioWithRate(1.5)
+        }
     }
     
     @IBAction func playChipmunk(sender: UIButton) {
-        playAudioWithVariablePitch(1000)
+        if (activeButton == sender) {
+            if (audioPlayerNode.playing) {
+                playbackPause()
+            } else {
+                playAudioWithVariablePitch(1000, reset: false)
+            }
+        } else {
+            playbackStop()
+            activeButton = sender
+            playAudioWithVariablePitch(1000, reset: true)
+        }
     }
     
     @IBAction func playDarthVader(sender: UIButton) {
-        playAudioWithVariablePitch(-1000)
+        if (activeButton == sender) {
+            if (audioPlayerNode.playing) {
+                playbackPause()
+            } else {
+                playAudioWithVariablePitch(-1000, reset: false)
+            }
+        } else {
+            playbackStop()
+            activeButton = sender
+            playAudioWithVariablePitch(-1000, reset: true)
+        }
     }
     
     @IBAction func playEcho(sender: UIButton) {
-        playAudioWithEcho()
+        if (activeButton == sender) {
+            if (audioPlayer.playing) {
+                playbackPause()
+            } else {
+                playAudioWithEcho()
+            }
+        } else {
+            playbackStop()
+            activeButton = sender
+            playAudioWithEcho()
+        }
     }
     
     @IBAction func playReverb(sender: UIButton) {
-        let alert = UIAlertController(title: "Reverb Presets", message: nil, preferredStyle: .ActionSheet)
-
-        // add actions
-        alert.addAction(UIAlertAction(title: "Small Room",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.SmallRoom)
-        }))
-        alert.addAction(UIAlertAction(title: "Medium Room",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.MediumRoom)
-        }))
-        alert.addAction(UIAlertAction(title: "Large Room",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.LargeRoom)
-        }))
-        alert.addAction(UIAlertAction(title: "Large Room 2",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.LargeRoom2)
-        }))
-        alert.addAction(UIAlertAction(title: "Medium Hall",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.MediumHall)
-        }))
-        alert.addAction(UIAlertAction(title: "Medium Hall 2",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.MediumHall2)
-        }))
-        alert.addAction(UIAlertAction(title: "Medium Hall 3",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.MediumHall3)
-        }))
-        alert.addAction(UIAlertAction(title: "Large Hall",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.LargeHall)
-        }))
-        alert.addAction(UIAlertAction(title: "Large Hall 2",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.LargeHall2)
-        }))
-        alert.addAction(UIAlertAction(title: "Plate",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.Plate)
-        }))
-        alert.addAction(UIAlertAction(title: "Medium Chamber",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.MediumChamber)
-        }))
-        alert.addAction(UIAlertAction(title: "Large Chamber",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.LargeChamber)
-        }))
-        alert.addAction(UIAlertAction(title: "Cathedral",
-            style: UIAlertActionStyle.Default,
-            handler: {(alert: UIAlertAction!) in
-                self.playAudioWithReverbPreset(.Cathedral)
-        }))
-        
-        //Present the AlertController
-        self.presentViewController(alert, animated: true, completion: nil)
+        if (activeButton == sender) {
+            if (audioPlayerNode.playing) {
+                playbackPause()
+            } else {
+                playAudioWithReverb(false)
+            }
+        } else {
+            playbackStop()
+            activeButton = sender
+            playAudioWithReverb(true)
+        }
     }
     
     @IBAction func playStop(sender: UIButton) {
-        playbackStop(true)
+        playbackStop()
     }
     
-//MARK: time slider
+//MARK: time slider action
     @IBAction func timeSliderChanged(sender: UISlider) {
         if (audioPlayer.playing) {
             audioPlayer.pause()
@@ -160,46 +160,50 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         
         let currentTime = (Double(timelineSlider.value) * audioPlayer.duration) / 100
         audioPlayer.currentTime = currentTime
+        echoPlayer.currentTime = currentTime
         trackAudio()
     }
-    
+
+//MARK: action implementations
     func playAudioWithRate(rate: Float) {
-        playbackStop(false)
-        
         audioUpdater = CADisplayLink(target: self, selector: Selector("trackAudio"))
         audioUpdater.frameInterval = 1
         audioUpdater.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
         
         audioPlayer.rate = rate
         audioPlayer.play()
+        
+        activeButton?.setImage(pauseButtonImage, forState: UIControlState.Normal)
     }
     
-    func playAudioWithVariablePitch(pitch: Float) {
-        playbackStop(false)
-        
-        let audioPlayerNode = AVAudioPlayerNode()
-        audioEngine.attachNode(audioPlayerNode)
-        
-        let changePitchEffect = AVAudioUnitTimePitch()
-        changePitchEffect.pitch = pitch
-        audioEngine.attachNode(changePitchEffect)
-        
-        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
-        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
-        
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: completionHandler)
+    func playAudioWithVariablePitch(pitch: Float, reset: Bool) {
+        if (reset) {
+            audioPlayerNode = AVAudioPlayerNode()
+            audioEngine.attachNode(audioPlayerNode)
+            
+            let changePitchEffect = AVAudioUnitTimePitch()
+            changePitchEffect.pitch = pitch
+            audioEngine.attachNode(changePitchEffect)
+            
+            audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+            audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+            
+            audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: completionHandler)
+            
+            audioUpdater = CADisplayLink(target: self, selector: Selector("trackAudio"))
+            audioUpdater.frameInterval = 1
+            audioUpdater.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
+        }
         
         try! audioEngine.start()
-        
         audioPlayerNode.play()
+        activeButton?.setImage(pauseButtonImage, forState: UIControlState.Normal)
     }
     
     /*
      * Code taken from http://sandmemory.blogspot.com/2014/12/how-would-you-add-reverbecho-to-audio.html
      */
     func playAudioWithEcho() {
-        playbackStop(false)
-        
         audioUpdater = CADisplayLink(target: self, selector: Selector("trackAudio"))
         audioUpdater.frameInterval = 1
         audioUpdater.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
@@ -207,47 +211,63 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         audioPlayer.play()
         
         let delay:NSTimeInterval = 0.1//100ms
-        var playtime:NSTimeInterval
-        playtime = echoPlayer.deviceCurrentTime + delay
+        let playtime = echoPlayer.deviceCurrentTime + delay
         echoPlayer.volume = 0.8;
         echoPlayer.playAtTime(playtime)
+        
+        activeButton?.setImage(pauseButtonImage, forState: UIControlState.Normal)
     }
     
-    func playAudioWithReverbPreset(preset: AVAudioUnitReverbPreset) {
-        playbackStop(false)
-        
-        let audioPlayerNode = AVAudioPlayerNode()
-        audioEngine.attachNode(audioPlayerNode)
-        
-        let unitReverb = AVAudioUnitReverb()
-        unitReverb.loadFactoryPreset(preset)
-        unitReverb.wetDryMix = 50.0
-        audioEngine.attachNode(unitReverb)
-        
-        audioEngine.connect(audioPlayerNode, to: unitReverb, format: nil)
-        audioEngine.connect(unitReverb, to: audioEngine.outputNode, format: nil)
-        
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: completionHandler)
+    /*
+     * Our teacher Kunal Chawla suggested to implement reverb via
+     * https://developer.apple.com/library/prerelease/ios/documentation/AVFoundation/Reference/AVAudioUnitReverb_Class/index.html
+     * instead of via http://sandmemory.blogspot.com/2014/12/how-would-you-add-reverbecho-to-audio.html
+     */
+    func playAudioWithReverb(reset: Bool) {
+        if (reset) {
+            audioPlayerNode = AVAudioPlayerNode()
+            audioEngine.attachNode(audioPlayerNode)
+            
+            let unitReverb = AVAudioUnitReverb()
+            unitReverb.loadFactoryPreset(.LargeChamber)
+            unitReverb.wetDryMix = 50.0
+            audioEngine.attachNode(unitReverb)
+            
+            audioEngine.connect(audioPlayerNode, to: unitReverb, format: nil)
+            audioEngine.connect(unitReverb, to: audioEngine.outputNode, format: nil)
+            
+            audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: completionHandler)
+            
+            audioUpdater = CADisplayLink(target: self, selector: Selector("trackAudio"))
+            audioUpdater.frameInterval = 1
+            audioUpdater.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
+        }
         
         try! audioEngine.start()
-        
         audioPlayerNode.play()
+        activeButton?.setImage(pauseButtonImage, forState: UIControlState.Normal)
     }
     
-    func playbackStop(reset: Bool) {
+    func playbackPause() {
+        audioPlayer.pause()
+        echoPlayer.pause()
+        audioEngine.pause()
+        audioPlayerNode.pause()
+        activeButton?.setImage(resumeButtonImage, forState: UIControlState.Normal)
+    }
+    
+    func playbackStop() {
         audioPlayer.stop()
-        if (reset) {
-            audioPlayer.currentTime = 0.0
-        }
+        audioPlayer.currentTime = 0.0
+        revertButtonImage()
+        activeButton = nil
         
         if (audioUpdater != nil) {
             audioUpdater.invalidate()
         }
         
         echoPlayer.stop()
-        if (reset) {
-            echoPlayer.currentTime = 0.0
-        }
+        echoPlayer.currentTime = 0.0
         
         audioEngine.stop()
         audioEngine.reset()
@@ -257,29 +277,51 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
     
 //MARK: AVAudioPlayerDelegate
     func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
-        playbackStop(true)
+        playbackStop()
     }
 
 //MARK: AudioPlayerNode completion handler
     func completionHandler() -> Void {
-        playbackStop(true)
+        playbackStop()
     }
 
 //MARK: player controls
     func resetPlaybackControls(duration: NSTimeInterval) {
-        timelineSlider.value = 0.0
-        startLabel.text = stringFromTimeInterval(0.0)
-        endLabel.text = stringFromTimeInterval(duration)
+        dispatch_async(dispatch_get_main_queue()) {
+            self.timelineSlider.value = 0.0
+            self.startLabel.text = self.stringFromTimeInterval(0.0)
+            self.endLabel.text = self.stringFromTimeInterval(duration)
+        }
     }
     
     func trackAudio() {
-        let normalizedTime = Float(audioPlayer.currentTime * 100.0 / audioPlayer.duration)
-        let startText = stringFromTimeInterval(audioPlayer.currentTime)
-        let endText = stringFromTimeInterval(audioPlayer.duration-audioPlayer.currentTime)
+        var currentTime:NSTimeInterval = 0
+        var duration:NSTimeInterval = 0
         
-        self.timelineSlider.value = normalizedTime
-        self.startLabel.text = startText
-        self.endLabel.text = endText
+        if (activeButton == slowButton ||
+            activeButton == fastButton ||
+            activeButton == echoButton) {
+            currentTime = audioPlayer.currentTime
+            duration = audioPlayer.duration
+        } else if (activeButton == chipmunkButton ||
+            activeButton == darthVaderButton ||
+            activeButton == reverbButton) {
+                let nodeTime = audioPlayerNode.lastRenderTime;
+                let playerTime = audioPlayerNode.playerTimeForNodeTime(nodeTime!)
+                let seconds = Double(playerTime!.sampleTime) / Double(playerTime!.sampleRate);
+                currentTime = seconds
+                duration = audioPlayer.duration
+        }
+        
+        let normalizedTime = Float(currentTime * 100.0 / duration)
+        let startText = stringFromTimeInterval(currentTime)
+        let endText = stringFromTimeInterval(duration-currentTime)
+
+        dispatch_async(dispatch_get_main_queue()) {
+            self.timelineSlider.value = normalizedTime
+            self.startLabel.text = startText
+            self.endLabel.text = endText
+        }
     }
 
 //MARK: Utility methods
@@ -293,6 +335,26 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
             return String(format: "%0.2d:%0.2d:%0.2d",hours,minutes,seconds)
         } else {
             return String(format: "%0.2d:%0.2d",minutes,seconds)
+        }
+    }
+    
+    func revertButtonImage() {
+        if let button = activeButton {
+            dispatch_async(dispatch_get_main_queue()) {
+                if (button == self.slowButton) {
+                    button.setImage(UIImage(named: "slowButton"), forState: UIControlState.Normal)
+                } else if (button == self.fastButton) {
+                    button.setImage(UIImage(named: "fastButton"), forState: UIControlState.Normal)
+                } else if (button == self.chipmunkButton) {
+                    button.setImage(UIImage(named: "chipmunkButton"), forState: UIControlState.Normal)
+                } else if (button == self.darthVaderButton) {
+                    button.setImage(UIImage(named: "darthVaderButton"), forState: UIControlState.Normal)
+                } else if (button == self.echoButton) {
+                    button.setImage(UIImage(named: "echoButton"), forState: UIControlState.Normal)
+                } else if (button == self.reverbButton) {
+                    button.setImage(UIImage(named: "reverbButton"), forState: UIControlState.Normal)
+                }
+            }
         }
     }
 }
